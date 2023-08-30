9e4536dabc1e610e7c5f044e8f9e2900bd1e6480
v-Chriz-v <190300375@ucaribe.edu.mx>
2023-08-30 13:42:48 -0500

    Proyecto de Esquema de comunicacion

--- Esquema de comunicacion/esquema.py
+++ Esquema de comunicacion/esquema.py
@@ -2,42 +2,42 @@ from pydub import AudioSegment
 import random
 
 # Cargar la canción
-song = AudioSegment.from_file("cancion.mp3", format="mp3")
+cancion = AudioSegment.from_file("cancion.mp3", format="mp3")
 
 # Convertir la canción a binario
-binary_data = song.raw_data
+datos_binarios = cancion.raw_data
 
 # Función de cifrado básica (XOR con una clave)
-def encrypt(data, key):
-    encrypted_data = bytearray()
+def encriptar(data, key):
+    datos_encriptados = bytearray()
     for byte in data:
-        encrypted_data.append(byte ^ key)
-    return encrypted_data
+        datos_encriptados.append(byte ^ key)
+    return datos_encriptados
 
 # Encriptar los datos binarios
-encryption_key = random.randint(0, 255)
-encrypted_data = encrypt(binary_data, encryption_key)
+llave_encriptacion = random.randint(0, 255)
+datos_encriptados = encriptar(datos_binarios, llave_encriptacion)
 
 # Simular ruido: variación de velocidad
-def simulate_noise(segment):
-    min_segment_duration = 1000  # Establece la duración mínima en milisegundos
-    if len(segment) < min_segment_duration:
+def simular_ruido(segment):
+    duracion_segmento = 1000  # Establece la duración mínima en milisegundos
+    if len(segment) < duracion_segmento:
         return segment
     speed_factor = random.uniform(0.3, 1.9)
     return segment.speedup(speed_factor)
 
 # Simular el canal y ruido
-segment_size = 10000
-encrypted_segments = [AudioSegment(data=encrypted_data[i:i+segment_size], frame_rate=song.frame_rate, sample_width=song.sample_width, channels=song.channels) for i in range(0, len(encrypted_data), segment_size)]
-noisy_segments = [simulate_noise(segment) for segment in encrypted_segments]
+tamaño_segmento = 10000
+segmentos_encriptados = [AudioSegment(data=datos_encriptados[i:i+tamaño_segmento], frame_rate=cancion.frame_rate, sample_width=cancion.sample_width, channels=cancion.channels) for i in range(0, len(datos_encriptados), tamaño_segmento)]
+noisy_segments = [simular_ruido(segment) for segment in segmentos_encriptados]
 
 # Función de descifrado básica
-def decrypt(data, key):
-    return encrypt(data, key)
+def desencriptar(data, key):
+    return encriptar(data, key)
 
 # Reconstruir la señal de audio
-reconstructed_data = bytes([byte ^ encryption_key for segment in noisy_segments for byte in segment.raw_data])
-reconstructed_song = AudioSegment(data=reconstructed_data, frame_rate=song.frame_rate, sample_width=song.sample_width, channels=song.channels)
+datos_reconstruidos = bytes([byte ^ llave_encriptacion for segment in noisy_segments for byte in segment.raw_data])
+cancion_reconstruida = AudioSegment(data=datos_reconstruidos, frame_rate=cancion.frame_rate, sample_width=cancion.sample_width, channels=cancion.channels)
 
 # Guardar la canción reconstruida en un archivo
-reconstructed_song.export("cancion_reconstruida.mp3", format="mp3")
+cancion_reconstruida.export("cancion_reconstruida.mp3", format="mp3")
